ffe3530b29963cda85a9664f69604d3e21527d23
helenwang <helenwang@squirtle.cs.uchicago.edu>
2015-03-02 15:43:30 -0600

    Initial Commit

---
+++ Align/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Align
+//
+//  Created by helenwang on 3/2/15.
+//  Copyright (c) 2015 helenwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Align/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Align
+//
+//  Created by helenwang on 3/2/15.
+//  Copyright (c) 2015 helenwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
